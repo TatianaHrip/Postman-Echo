65e37c60020ca4f4db350262a88ba15d8fc815a7
Tatyana Hripkova <tkhripkova@bk.ru>
2021-02-04 23:14:54 +0200

    тест проходит

--- src/test/java/ru/netology/rest/MobileBankApiTestPost.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestPost.java
@@ -18,7 +18,7 @@ class MobileBankApiTestPost {
                 .post("/post")
                 .then()// Проверки
                 .statusCode(200)
-                .body("data", equalTo("Ну, Добрый день"));
+                .body("data", equalTo("Добрый день"));
     }
 }
 
